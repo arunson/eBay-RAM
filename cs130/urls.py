ec50b8ae16ff2b0f93fd14a441e1b7ddbc0875dc
Keith Chen <randombum@gmail.com>
2011-05-01 21:33:16 -0700

    Initial application commit.
    A search page can be seen but it doesn't do anything at the moment. Supply your own settings.py page to run.

---
+++ cs130/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls.defaults import patterns, include, url
+from cs130.eram.views import search
+
+
+# QUICK AND DIRTY IMPORT FOR DEV ENVIRONMENTS TO SERVE STATIC FILES
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('', 
+    (r'^$', search),
+    (r'^search/$', search),
+)
+
+    # Examples:
+    # url(r'^$', 'cs130.views.home', name='home'),
+    # url(r'^cs130/', include('cs130.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+# QUICK AND DIRTY IMPORT FOR DEV ENVIRONMENTS TO SERVE STATIC FILES
+urlpatterns += staticfiles_urlpatterns()
