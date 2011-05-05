f60ba65ddf88491f20ed8c8427bfbe6e20770304
qewava <qewava@qewava-laptop.(none)>
2011-05-05 00:12:30 -0700

    added ebay module, change prototype to include pictures and links

--- cs130/eram/views.py
+++ cs130/eram/views.py
@@ -1,33 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from cs130.eram.forms import SearchForm
-import json, urllib
+from cs130.eram.other_modules import ebay_module
+import os
     
 def search(request):
 	if 'q' in request.GET and request.GET['q']:
-		query = urllib.quote(request.GET['q'])
-		api_site = "http://svcs.ebay.com/services/search/FindingService/v1"
-		version = "1.9.0" 
-		app_id = "YOUR ID HERE" 
-		global_id = "EBAY-US"
-		response_format = "JSON"
-		num_responses = "20"
-
-		apiurl = api_site + "?"
-		apiurl += "OPERATION-NAME=findItemsByKeywords"
-		apiurl += "&SERVICE-VERSION=" + version
-		apiurl += "&SECURITY-APPNAME=" + app_id
-		apiurl += "&RESPONSE-DATA-FORMAT=" + response_format
-		apiurl += "&GLOBAL-ID=" + global_id
-		apiurl += "&keywords=" + query
-		apiurl += "&paginationInput.entriesPerPage=" + num_responses
-		fd = urllib.urlopen(apiurl)
-		string_response = fd.read()
-		json_response = json.loads(string_response)
+		ebay_communicator = ebay_module.EbayInterface(os.getcwd() + '/eram/module_config.cfg')
+		item_ids = ebay_communicator.search(request.GET['q'], 20)
 		result = "Results: <p>"
-		for i in json_response["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"] :
-			result += i["title"][0] + "<p>"
-		fd.close()	
+		for item_id in item_ids :
+			item_info = ebay_communicator.get_item_info_by_id(item_id)
+			try:
+				result += "<img src=\"" + item_info["galleryURL"] + "\"\\>"
+			except KeyError:
+				result += "NO PIC"
+			result += "    <a href=\"" + item_info["viewItemURL"] + "\">" + item_info["title"] + "</a>"
+			result += "<p>"
 		return HttpResponse(result)
 	else:
 		search_form = SearchForm()
@@ -52,4 +41,3 @@ def ip_location(request):
     #time_zone = location_info["timeZone"]
     
     return render_to_response('ip_location.html', location_info)
-
--- cs130/urls.py
+++ cs130/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from cs130.eram.views import search
+from cs130.eram.views import search, ip_location
 
 
 # QUICK AND DIRTY IMPORT FOR DEV ENVIRONMENTS TO SERVE STATIC FILES
