f04a935980edf24c0165ab5d444b03f6b890888a
qewava <qewava@qewava-laptop.(none)>
2011-05-12 20:25:49 -0700

    updated productwiki module to include mpn and upc capabilities

--- cs130/eram/review_modules/productwiki_module.py
+++ cs130/eram/review_modules/productwiki_module.py
@@ -1,6 +1,7 @@
 import json, urllib, ConfigParser
 import review_module
 class ProductwikiInterface:
+	supported_types = ["upc", "mpn", "title"]
 	# __init__(self, config_location)
 	# input: location of configuration file
 	def __init__(self, config_location):
@@ -16,18 +17,16 @@ class ProductwikiInterface:
 	# output: tuple containing total score (below 100) and number of reviews
 	# ---not thoroughly tested---
 	# ---not robust---
-	def get_score(self, query):
-		url_query = urllib.quote(query)
-	
-		apiurl = self.api_site + "?"
-		apiurl += "op=search"
-		apiurl += "&q=" + url_query 
-		apiurl += "&format=" + self.response_format
-		apiurl += "&key=" + self.api_key
-		print apiurl
-		# a lot of errors that could come up here, have to deal with them at some point
-		
-		fd = urllib.urlopen(apiurl)
+	def get_score(self, query, query_type):
+		if (query_type not in self.supported_types) :
+			return (-1, -1)
+		if (query_type == "title") :
+			return self.get_score_by_search(query)
+		if (query_type == "upc" or query_type == "mpn") :
+			return self.get_score_by_product(query, query_type)
+
+	def get_score_by_url(self, api_url) :
+		fd = urllib.urlopen(api_url)
 		string_response = fd.read()
 		fd.close()	
 
@@ -42,3 +41,23 @@ class ProductwikiInterface:
 
 		return (first_product["proscore"], first_product["number_of_reviews"])
 	
+
+	def get_score_by_product(self, query, query_type) :
+		url_query = urllib.quote(query)
+		api_url = self.api_site + "?"
+		api_url += "op=product"
+		api_url += "&format=" + self.response_format
+		api_url += "&idtype=" + query_type
+		api_url += "&idvalue=" + url_query
+		api_url += "&key=" + self.api_key
+		return self.get_score_by_url(api_url)
+
+	def get_score_by_search(self, query) :
+		url_query = urllib.quote(query)
+		api_url = self.api_site + "?"
+		api_url += "op=search"
+		api_url += "&q=" + url_query 
+		api_url += "&format=" + self.response_format
+		api_url += "&key=" + self.api_key
+		return self.get_score_by_url(api_url)
+		
